98af4f1e2cc83c361665076673efbad9732ea228
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-26 14:17:50 -0600

    Create curated lists controller with show and create actions

---
+++ app/controllers/curated_lists_controller.rb
@@ -0,0 +1,21 @@
+class CuratedListsController < ApplicationController
+  def show
+    found_list = CuratedList.where('user_id = ? AND slug = ?', id_for_username(params[:username]), params[:slug]).first.to_json
+    @curated_list = CuratedListSerializer.new([found_list]).to_json
+  end
+
+  def create
+    user = User.find(session_current_user_id)
+    new_list = user.curated_lists.new(strong_params)
+  end
+
+  private
+
+  def id_for_username(username)
+    User.find_by(username: username).id
+  end
+
+  def strong_params
+    params.permit(:name, :description)
+  end
+end

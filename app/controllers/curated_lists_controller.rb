9da504157c3ac5f47c01feffa3fedc167403a238
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-27 11:45:14 -0600

    Complete logic of creating created list

--- app/controllers/curated_lists_controller.rb
+++ app/controllers/curated_lists_controller.rb
@@ -1,15 +1,17 @@
 class CuratedListsController < ApplicationController
   def show
-    found_list = CuratedList.where('user_id = ? AND slug = ?', id_for_username(params[:username]), params[:slug]).first.to_json
+    found_list = CuratedList.where("user_id = ? AND slug = ?", id_for_username(params[:username]), params[:slug]).first.to_json
     @curated_list = CuratedListSerializer.new([found_list]).to_json
   end
 
   def create
     user = User.find(session_current_user_id)
     new_list = user.curated_lists.new(strong_params)
-    if new_list.save
-      flash[:success] = 'Hooray!'
-    end
+    render json: if new_list.save
+                   new_list.to_json
+                 else
+                   new_list.errors.to_json
+                 end
   end
 
   private
@@ -19,6 +21,6 @@ class CuratedListsController < ApplicationController
   end
 
   def strong_params
-    params.permit(:name, :description)
+    params.permit(:name, :description, :slug)
   end
 end

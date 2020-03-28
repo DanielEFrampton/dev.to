21869cb92279d35e1fcdccc0e95de6299a63bf19
Noah Gambee <noah@gambee.dev>
2020-03-28 15:39:31 -0600

    Style delete button (#63)
    * Fetch ListData and pass as props to CuratedList component
    
    * Render list title onto dom
    
    * Add articles array to curatedList data object
    
    * Add toggleVisbility method to display or hide NewListForm
    
    * Add ternary operator to toggle words on add form button
    
    * Add scss file for curated list form and style button
    
    * Style curated title names underneath form
    
    * Style form
    
    * Change order of form elements
    
    * Add articles count to the end of each curated list title
    
    * Add check for empty curated list title upon new form submission
    
    * Render each article within a curated list by creating curatedListElements variable
    
    * Add remove-article button to card and style it
    
    * Add wrapper jsx element around article cards and add border
    
    * Create back-to-list button and style it
    
    * Add check for current url to make sure curatedList is not rendered on the wrong pages
    
    * Fix .then statement that renders readingList after deletion of a list
    
    * Style 'delete-list' button and move it to the bottom of the page
    
    * Add curated list description to list show page

--- app/javascript/readingList/CuratedList.jsx
+++ app/javascript/readingList/CuratedList.jsx
@@ -32,7 +32,7 @@ export class CuratedList extends Component {
       },
       credentials: 'same-origin',
     }).then(
-      window.location.pathname = '/readinglist'
+      () => window.location.pathname = '/readinglist'
     );
   };
 
@@ -62,16 +62,17 @@ export class CuratedList extends Component {
       <h1>
         {this.props.curatedListData.name}
       </h1>
+      <h3>{this.props.curatedListData.description}</h3>
       <a className='back-to-list-btn' href='/readinglist'>Back to Reading List</a>
+      <section className='curated-cards'>
+          {curatedCards}
+      </section>
       <button
         className='delete-list-button'
         onClick={() => this.deleteList()}
       >
         Delete List
       </button>
-      <section className='curated-cards'>
-          {curatedCards}
-      </section>
     </div>
     )
   }

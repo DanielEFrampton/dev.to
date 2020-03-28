12551c6ba049ba572761051789b5c627696d420f
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-28 14:14:52 -0600

    Add test expectation of and configuration for dependent destroy of join objects upon curated list deletion

--- app/models/curated_list.rb
+++ app/models/curated_list.rb
@@ -2,6 +2,6 @@ class CuratedList < ApplicationRecord
   validates :name, :description, :slug, presence: true
 
   belongs_to :user
-  has_many :curated_list_articles
+  has_many :curated_list_articles, dependent: :destroy
   has_many :articles, through: :curated_list_articles
 end
